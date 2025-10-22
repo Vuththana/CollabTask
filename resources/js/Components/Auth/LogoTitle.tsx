81a67717b66fd60c9579ef7f949ec550a1a98d4c
Vuththana <135146361+Vuththana@users.noreply.github.com>
2025-10-23 04:31:44 +0700

    fixed directory

---
+++ resources/js/Components/Auth/LogoTitle.tsx
@@ -0,0 +1,15 @@
+const LogoTitle = () => {
+    return(
+    <div
+        className="inline-flex h-16 w-16 items-center justify-center rounded-xl mb-4 bg-gradient-to-tl from-purple-400 bg-purple-700 font-extrabold"
+    >
+        <span
+        className=" text-white text-xl"
+        >
+            C
+        </span>
+    </div>
+    );
+};
+
+export default LogoTitle;
--- routes/web.php
+++ routes/web.php
@@ -9,14 +9,18 @@ Route::get('/', action: function () {
     return Inertia::render('Dashboard/Admin/Index');
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get('/login', action: function () {
+    return Inertia::render('Auth/Login');
 });
 
-require __DIR__.'/auth.php';
+// Route::get('/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// require __DIR__.'/auth.php';
